1cb02e30f84586c9b866e8fb2316a700098753c5
Xabier Rodriguez Calvar <xrcalvar@igalia.com>
2009-10-07 16:50:40 +0200

    Fixed a warning because of a missing prototype. Function made private.

--- mafw-lastfm/mafw-lastfm-scrobbler.c
+++ mafw-lastfm/mafw-lastfm-scrobbler.c
@@ -374,8 +374,8 @@ scrobble_timeout (gpointer data)
   return FALSE;
 }
 
-void
-mafw_lastfm_scrobbler_clean_queue (MafwLastfmScrobbler *scrobbler)
+static void
+clean_queue (MafwLastfmScrobbler *scrobbler)
 {
   glong timestamp = time (NULL);
   MafwLastfmTrack *track;
@@ -402,7 +402,7 @@ mafw_lastfm_scrobbler_flush_queue (MafwLastfmScrobbler *scrobbler)
 {
   if (scrobbler->priv->timeout != 0) {
     g_source_remove (scrobbler->priv->timeout);
-    mafw_lastfm_scrobbler_clean_queue (scrobbler);
+    clean_queue (scrobbler);
     scrobble_timeout ((gpointer) scrobbler);
   }
 }
